2a5a78c7995b9814a2e996a55542379b973c86ac
Harriet-J <seremharriet@gmail.com>
2024-09-07 08:30:05 -0700

    task 1

--- main.c
+++ main.c
@@ -2,16 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
 
 int main(void)
 {
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
+    pid_t pid;
+    int status;
 
     while (1)
     {
         printf("#cisfun$ ");
+        fflush(stdout); /* Ensure prompt is printed immediately */
         nread = getline(&line, &len, stdin);
 
         if (nread == -1)
@@ -20,14 +26,38 @@ int main(void)
             exit(EXIT_SUCCESS);
         }
 
-        if (strcmp(line, "exit\n") == 0)
+        /* Remove newline character from input */
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
+
+        if (strcmp(line, "exit") == 0)
         {
             free(line);
             exit(EXIT_SUCCESS);
         }
 
-        system(line);
+        pid = fork();
+
+        if (pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0)
+        {
+            /* Child process */
+            execlp(line, line, (char *)NULL);
+            perror(line);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            /* Parent process */
+            waitpid(pid, &status, 0);
+        }
     }
     return 0;
 }
+
 
